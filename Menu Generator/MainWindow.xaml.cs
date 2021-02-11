6c899f322a5029f9fdd12e012d408810c7cb550f
Baranga Viktor <viktorbaranga@yahoo.com>
2021-02-11 15:35:54 +0200

    Add project files.

---
+++ Menu Generator/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeMenu();
+        }
+
+        private void MakeMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[]
+                    {
+                        "Plain Bagel",
+                        "Onion Bagel",
+                        "Pumpernicke Bagel",
+                        "Everything Bagel"
+                    };
+                }
+                menuItems[i].GenerateMenuItem();
+            }
+
+            TextBlock[] items = new TextBlock[] { item1, item2, item3, item4, item5 };
+            TextBlock[] prices = new TextBlock[] { price1, price2, price3, price4, price5 };
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                items[i].Text = menuItems[i].Description;
+                prices[i].Text = menuItems[i].Price;
+            }
+        }
+    }
+}
---
+++ Menu Generator/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_Generator
+{
+    class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+
+        public string[] Proteins = { "Roast Beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+
+        public string[] Condiments = {"Yellow Mustard", "Brown Mustard", "Honey Mustard", "Mayo",
+                        "Relish", "French Dressing"};
+
+        public string[] Breads = { "Rye", "White", "Wheat", "Pumpernicke", "A Roll" };
+
+        public string Description = "";
+
+        public string Price;
+
+        public void GenerateMenuItem()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+
+            Price = price.ToString("c");
+        }
+    }
+}
